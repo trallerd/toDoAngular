663f0cc6e8c35b15cb9abbf862d41f62af8ec5e6
trallerd <jessicacristec@gmail.com>
2021-06-23 20:40:50 -0300

    remove && done tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,25 @@ export class AppComponent {
   public title: String = 'Tarefas do dia';
 
   constructor() {
-    this.todos.push(new Todo(1,'Tarefa 1', false));    
-    this.todos.push(new Todo(2,'Tarefa 2', false));    
-    this.todos.push(new Todo(3,'Tarefa 3', true));    
+    this.todos.push(new Todo(1, 'Tarefa 1', false));
+    this.todos.push(new Todo(2, 'Tarefa 2', false));
+    this.todos.push(new Todo(3, 'Tarefa 3', true));
   }
 
   aletrarTexto() {
     this.title = 'Muda Texto GG'
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+  unMark(todo: Todo) {
+    todo.done = false;
   }
 }
