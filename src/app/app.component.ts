cd2d4de803bdf5dd76e868059a176154f5be1dff
trallerd <jessicacristec@gmail.com>
2021-06-23 20:16:21 -0300

    todo p1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+  public title: String = 'Tarefas do dia';
+
+  constructor() {
+    this.todos.push('Tarefa 01');    
+    this.todos.push('Tarefa 02');    
+    this.todos.push('Tarefa 03');    
+  }
+
+  aletrarTexto() {
+    this.title = 'Muda Texto GG'
+  }
 }
