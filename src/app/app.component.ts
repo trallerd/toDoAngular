a86ff5da4c6f88a6ac3b76a8866944a98c983001
trallerd <jessicacristec@gmail.com>
2021-06-23 21:04:19 -0300

    crud Todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,11 +10,27 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = [];
   public title: String = 'Tarefas do dia';
+  public form: FormGroup;
 
-  constructor() {
-    this.todos.push(new Todo(1, 'Tarefa 1', false));
-    this.todos.push(new Todo(2, 'Tarefa 2', false));
-    this.todos.push(new Todo(3, 'Tarefa 3', true));
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(30),
+        Validators.required
+      ])]
+    });
+  }
+
+  addTodo() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   aletrarTexto() {
